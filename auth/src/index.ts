6cf98de41dec27dac217e322f3d3c828efc2135c
Semih Orhan <sorhan3.14@gmail.com>
2024-03-24 12:41:09 +0300

    type check user props

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -27,14 +27,15 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
-};
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
 start();
