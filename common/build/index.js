014182cc145c2d4af3dc586a290b019c7d8a5e63
Semih Orhan <sorhan3.14@gmail.com>
2024-04-01 15:24:47 +0300

    easy publish command

---
+++ common/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const color = {
+    red: 20,
+    blue: 10,
+    green: 10,
+};
+console.log(color);
+exports.default = color;
